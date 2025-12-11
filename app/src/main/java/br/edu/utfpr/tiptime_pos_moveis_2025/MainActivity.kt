ef6eab709ebb2e20c30791e3f49848cb621a7fa4
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2025-12-11 18:17:29 -0300

    Correções na Tela

--- app/src/main/java/br/edu/utfpr/tiptime_pos_moveis_2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/tiptime_pos_moveis_2025/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.edu.utfpr.tiptime_pos_moveis_2025
-import android.icu.text.NumberFormat
+
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.tiptime_pos_moveis_2025.databinding.ActivityMainBinding
-
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,40 +29,37 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             calculateTip()
         }
-        val formattedTip: String = NumberFormat.getCurrencyInstance().format(0)
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(0)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-    }
+
+    } //fim do onCreate()
 
     private fun calculateTip() {
 
-        //Entrada
+        //entrada
         val stringInTextField = binding.costOfService.text.toString()
-        val cost : Double? = stringInTextField.toDoubleOrNull()?: return
 
+        val cost = stringInTextField.toDoubleOrNull() ?: return
 
         val selectedId = binding.tipOption.checkedRadioButtonId
-        val tipPercentage: Double = when (selectedId) {
+        val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
-            R.id.option_fifteen_percent -> 0.15
             else -> 0.15
         }
 
-
-        //Processamento
-        var tip : Double = cost!! * tipPercentage
+        //processamento
+        var tip = cost * tipPercentage
         val roundUp = binding.roundUpSwitch.isChecked
 
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
 
-
-        //Saida
-        val formattedTip: String = NumberFormat.getCurrencyInstance().format(tip)
+        //saído
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
-
     }
 
-}
+}//Fim do MainActivity
